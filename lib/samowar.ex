7cd83021fcd552acb27e5b5748cd58de26497b52
ddidwyll <ddidwyll@gmail.com>
2024-11-19 15:02:37 +0400

    Add phoenix

--- lib/samowar.ex
+++ lib/samowar.ex
@@ -1,18 +1,9 @@
 defmodule Samowar do
   @moduledoc """
-  Documentation for `Samowar`.
+  Samowar keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Samowar.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
